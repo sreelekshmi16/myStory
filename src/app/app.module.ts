9d5ec5e0007b171fb5997ea9a2f0f7574dd5e233
sreelekshmi16 <sreelekshmi.gs@experionglobal.com>
2019-09-20 10:06:48 +0530

    webpage updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    ProductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
